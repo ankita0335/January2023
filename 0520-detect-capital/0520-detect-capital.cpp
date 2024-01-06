1ed3b00d834f228093520dd9b5325e558e98ecf1
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-01-06 18:22:31 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (55.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
-    bool allcapital(string word){
-        for(char i:word){
-            if(i<'A' || i>'Z'){
+    bool allcapital(string word)
+    {
+        for(char i:word)
+        {
+            if(i<'A' || i>'Z')
+            {
                 return false;
             }
         }
         return true;
     }
-    bool allsmall(string word){
-        for(char i:word){
-            if(i<'a' || i>'z'){
+    bool allsmall(string word)
+    {
+        for(char i:word)
+        {
+            if(i<'a' || i>'z')
+            {
                 return false;
             }
         }
         return true;
     }
-    bool detectCapitalUse(string word) {
-        if(allcapital(word) || allsmall(word) || allsmall(word.substr(1))){
+    bool allsmallexclude1(string word) 
+    {
+        if (word[0] >= 'A' && word[0] <= 'Z') 
+        {
+            for (int i = 1; i < word.size(); i++) 
+            {
+                if (word[i] < 'a' || word[i] > 'z') 
+                {
+                    return false;
+                }
+            }
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
+    }
+    bool detectCapitalUse(string word) 
+    {
+        if(allcapital(word) || allsmall(word) || allsmallexclude1(word))
+        {
             return true;    
         }
         return false;
