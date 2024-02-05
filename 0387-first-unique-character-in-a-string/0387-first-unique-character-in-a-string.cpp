f9af028404f7aee0abfeeec424260a5cc1749487
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-02-05 16:25:55 +0530

    Time: 38 ms (35.62%), Space: 13 MB (17.78%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -8,8 +8,10 @@ public:
         }
         for(int i=0;i<s.length();i++)
         {
-            if(mp[s[i]]==1)
+            if(mp[s[i]]==1){
                 return i;
+                break;
+            }
         }
         return -1;
     }
